77bd354de66e244b60a1d8f63a61bd5243ce2ab1
Thomas Marconi <thomas.marconi@outlook.com>
2023-10-08 05:41:35 +0000

    complete

--- website/website.py
+++ website/website.py
@@ -2,17 +2,23 @@ from flask import Flask, render_template
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 @app.route("/")
-def home():
-    return render_template("home.html")
-@app.route("/about")
-def about():
-    return render_template("about.html")
+def portfolio():
+    return render_template("portfolio.html")
+@app.route("/aboutme")
+def aboutme():
+    return render_template("aboutme.html")
 @app.route("/info")
 def info():
     return render_template("info.html")
+@app.route("/about")
+def about():
+    return render_template("/articles/about.html")
 @app.route("/working_with_my_first_client")
 def working_with_my_first_client():
-    return render_template("working_with_my_first_client.html")
+    return render_template("/articles/working_with_my_first_client.html")
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
