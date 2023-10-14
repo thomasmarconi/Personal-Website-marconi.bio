05c943b88be6c4bdf7c585b789a16acaa12b7a1a
thomasmarconi <thomas.marconi@outlook.com>
2023-10-13 21:39:27 -0400

    changed contact form for the meanwhile

--- website/static/js/portfolio.js
+++ website/static/js/portfolio.js
@@ -24,3 +24,24 @@ const appearOnScroll = new IntersectionObserver(function(entries) {
 });
 
 faders.forEach((el) => appearOnScroll.observe(el)); 
+
+// const sendEmailButton = document.getElementById('send-email');
+// const contactForm = document.getElementById('contact-form');
+
+// sendEmailButton.addEventListener('click', function () {
+//     console.log("clicked");
+//     const email = 'thomas.marconi@outlook.com'; // Replace with your email address
+//     const subject = `Contact from Thomas Marconi's Portfolio`;
+//     const formData = new FormData(contactForm);
+
+//     let body = 'Message from: ' + formData.get('name') + '\n';
+//     body += 'Email: ' + formData.get('email') + '\n';
+//     body += 'Subject: ' + formData.get('subject') + '\n';
+//     body += 'Message: ' + formData.get('message');
+//     console.log(body);
+//     body = encodeURIComponent(body);
+//     console.log(body);
+//     const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${body}`;
+//     console.log(mailtoLink);
+//     window.location.href = mailtoLink;
+// });
